14c834bcf92a535afc147ee1acce3921549277b1
Gianpaolo Macario <gmacario@gmail.com>
2009-11-29 19:59:56 +0100

    Search git-ccm in JJOHNNY_GITCCM_HOME

--- src/runme-gmacario.sh
+++ src/runme-gmacario.sh
@@ -21,7 +21,7 @@ CCM_HOME=/cygdrive/c/Programmi/Telelogic/Telelogic\ Synergy\ 7.0
 CCM_USER=macario
 #CCM_PASS=xxxxx
 engine_hostname=unfs06.venaria.marelli.it
-project_spec=/projects/TRAINING70
+project_spec=/project/TRAINING70
 
 # Variables as per http://maven.apache.org/scm/synergy.html
 database_delimiter=-
@@ -32,21 +32,66 @@ project_version=Release_1.7.0_20080701
 #release=
 #purpose=
 
-MAINLINE_HOME=${HOME}/MYGIT/mainline
+JJOHNNY_GITCCM_HOME=${HOME}/MYGIT/jjohnny-git-ccm-clone
 
 # ----------------------------------------------------------------------------
 # Sanity checks, default parameters, etc
 #
 . ./syn_common.sh
 
-which git-ccm 2>/dev/null || PATH="${MAINLINE_HOME}:${PATH}"
+which git-ccm 2>/dev/null || PATH="${JJOHNNY_GITCCM_HOME}:${PATH}"
 
 # ----------------------------------------------------------------------------
 echo "DBG: PATH=${PATH}"
 echo "DBG: CCM_USER=${CCM_USER}"
 echo "DBG: CCM_PASS=${CCM_PASS}"
 
+export CCM_USER CCM_PASS
+
+# Try one of the following:
+#
+#  git-ccm init --help
+#  git-ccm clone --help
+#git-ccm
+
+# git-ccm [options]
+# Options:
+#    -h, --help				show this help message and exit
+#	 -s SERVER, --server=SERVER
+#							resolvable name/IP and the ccm server.
+#	 -d DB, --database=DB	database path on the ccm host
 #git-ccm init --help
-git-ccm init --server=${engine_hostname} --database=${project_spec}
+
+#git-ccm init \
+#		--server=${engine_hostname} \
+#		--database=${project_spec}
+
+#Usage: git-ccm [options]
+#
+#Options:
+#  -h, --help            show this help message and exit
+#  -s SERVER, --server=SERVER
+#                        resolvable name/IP of the ccm server.
+#  -d DB, --database=DB  database path on the ccm host
+#  -p PROJECT, --project=PROJECT
+#                        Project name~version to checkout
+#  -r RELEASE, --release=RELEASE
+#                        release name of the project
+#  -c COMMENT, --comment=COMMENT
+#                        Initial comment for checking out
+#git-ccm clone --help
+
+set -x
+
+#GIT_CCM="python -m pdb ${JJOHNNY_GITCCM_HOME}/git-ccm"
+GIT_CCM=git-ccm
+
+# DBG DBG DBG
+#export CCM_WORKAREA=D:/temp
+
+${GIT_CCM} clone \
+		--server=${engine_hostname} \
+		--database=${project_spec} \
+		--project=${project_name}${database_delimiter}${project_version}
 
 # === EOF ===
